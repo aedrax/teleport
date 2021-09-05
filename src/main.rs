470b4b23e63be341da78671a2a5a2f5b5bbdf4ff
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2021-09-05 16:33:11 -0400

    serializing struct to json for additional metadata

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::io::Result;
 use std::io::{self, Read, Write};
 use std::net::Ipv4Addr;
 use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::os::unix::ffi::OsStrExt;
 use std::path::PathBuf;
+use std::str;
 use structopt::StructOpt;
+//use serde::ser::{Serialize, Serializer, SerializeStruct};
+use serde::{Deserialize, Serialize};
 
 /// Teleport is a simple application for sending files from Point A to Point B
 
@@ -24,11 +26,18 @@ struct Opt {
     port: u16,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct TeleProto {
+    filenum: u64,
+    totalfiles: u64,
+    filesize: u64,
+    filename: String,
+}
+
 /// ACK response type when filename is received and ready to receive file data
 const ACK: [u8; 3] = ['A' as u8, 'C' as u8, 'K' as u8];
 
 fn main() {
-
     // Process arguments
     let opt = Opt::from_args();
 
@@ -46,12 +55,19 @@ fn main() {
 /// Client function sends filename and file data for each filepath
 fn client(opt: Opt) -> Result<()> {
     // For each filepath in the input vector...
-    for item in opt.input {
+    for (num, item) in opt.input.iter().enumerate() {
         let filepath = item.to_str().unwrap();
         let filename = item.file_name().unwrap();
 
         // Validate file
         let file = File::open(&filepath).expect("Failed to open file");
+        let meta = file.metadata().expect("Failed to read metadata");
+        let header = TeleProto {
+            filenum: (num + 1) as u64,
+            totalfiles: opt.input.len() as u64,
+            filesize: meta.len(),
+            filename: filename.to_str().unwrap().to_string(),
+        };
 
         // Connect to server
         let addr = format!("{}:{}", opt.dest, opt.port);
@@ -61,22 +77,28 @@ fn client(opt: Opt) -> Result<()> {
         )
         .expect(&format!("Error connecting to: {:?}", opt.dest));
 
-        print!("Sending file: {:?} ...", filename);
-        io::stdout().flush().unwrap();
+        println!(
+            "Sending file {}/{}: {:?}",
+            header.filenum, header.totalfiles, header.filename
+        );
 
-        // Send filename first
-        stream.write(&filename.as_bytes()).expect("Failed to write to stream");
+        // Send header first
+        let serial = serde_json::to_string(&header).unwrap();
+        //let serial = &TeleProto::serialize(&header).unwrap();
+        stream
+            .write(&serial.as_bytes())
+            .expect("Failed to write to stream");
 
         // Send file data
-        let _ = send(stream, file);
+        let _ = send(stream, file, header);
 
-        println!("done!");
+        println!(" done!");
     }
     Ok(())
 }
 
 /// Send function receives the ACK for data and sends the file data
-fn send(mut stream: TcpStream, mut file: File) -> Result<()> {
+fn send(mut stream: TcpStream, mut file: File, header: TeleProto) -> Result<()> {
     let mut buf: [u8; 4096] = [0; 4096];
 
     // Receive ACK that the server is ready for data
@@ -88,6 +110,7 @@ fn send(mut stream: TcpStream, mut file: File) -> Result<()> {
     }
 
     // Send file data
+    let mut sent = 0;
     loop {
         // Read a chunk of the file
         let len = file.read(&mut buf).expect("Failed to read file");
@@ -104,6 +127,14 @@ fn send(mut stream: TcpStream, mut file: File) -> Result<()> {
             println!("Error sending data");
             break;
         }
+
+        sent += len;
+        let percent: f64 = (sent as u64 / header.filesize) as f64 * 100f64;
+        print!(
+            "\r => {} of {} bytes ({:.02}%)",
+            sent, header.filesize, percent
+        );
+        io::stdout().flush().unwrap();
     }
 
     Ok(())
@@ -127,29 +158,32 @@ fn server(opt: Opt) -> Result<()> {
 /// Recv receives filenames and file data for a file
 fn recv(mut stream: TcpStream) -> Result<()> {
     let ip = stream.peer_addr().unwrap();
-    println!("New connection from: {}", ip);
 
-    // Receive filename first
+    // Receive header first
     let mut name_buf: [u8; 4096] = [0; 4096];
     let len = stream.read(&mut name_buf)?;
     let fix = &name_buf[..len];
-    let filename = std::str::from_utf8(&fix).expect("Cannot understand filename");
-    print!("Receiving: {} ...", filename);
-    io::stdout().flush().unwrap();
+    let header: TeleProto =
+        serde_json::from_str(str::from_utf8(&fix).unwrap()).expect("Cannot understand filename");
+    println!(
+        "Receiving file {}/{}: {:?} (from {})",
+        header.filenum, header.totalfiles, header.filename, ip
+    );
 
     // Send ready for data ACK
     stream.write(&ACK).expect("Failed to ACK");
 
     // Receive file data
-    let mut file = File::create(&filename).expect("Could not open file");
+    let mut file = File::create(&header.filename).expect("Could not open file");
     let mut buf: [u8; 4096] = [0; 4096];
+    let mut received = 0;
     loop {
         // Read from network connection
         let len = stream.read(&mut buf).expect("Failed to read");
 
         // A receive of length 0 means the transfer is complete
         if len == 0 {
-            println!("done!");
+            println!(" done!");
             break;
         }
 
@@ -157,9 +191,17 @@ fn recv(mut stream: TcpStream) -> Result<()> {
         let data = &buf[..len];
         let wrote = file.write(data).expect("Failed to write to file");
         if len != wrote {
-            println!("Error writing to file: {}", filename);
+            println!("Error writing to file: {}", header.filename);
             break;
         }
+
+        received += len;
+        let percent: f64 = (received as u64 / header.filesize) as f64 * 100f64;
+        print!(
+            "\r => {} of {} bytes ({:.02}%)",
+            received, header.filesize, percent
+        );
+        io::stdout().flush().unwrap();
     }
 
     Ok(())
