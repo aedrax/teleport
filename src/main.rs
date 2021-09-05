1aaec99a7902e18b7077c74779bd8c6d90b6ced1
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2021-09-05 17:33:01 -0400

    nicer transfer print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Result;
 use std::io::{self, Read, Write};
@@ -6,8 +7,6 @@ use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::path::PathBuf;
 use std::str;
 use structopt::StructOpt;
-//use serde::ser::{Serialize, Serializer, SerializeStruct};
-use serde::{Deserialize, Serialize};
 
 /// Teleport is a simple application for sending files from Point A to Point B
 
@@ -32,6 +31,13 @@ struct TeleProto {
     totalfiles: u64,
     filesize: u64,
     filename: String,
+}
+
+struct SizeUnit {
+    partial: f64,
+    partial_unit: char,
+    total: f64,
+    total_unit: char,
 }
 
 /// ACK response type when filename is received and ready to receive file data
@@ -129,10 +135,11 @@ fn send(mut stream: TcpStream, mut file: File, header: TeleProto) -> Result<()> 
         }
 
         sent += len;
-        let percent: f64 = (sent as u64 / header.filesize) as f64 * 100f64;
+        let percent: f64 = (sent as f64 / header.filesize as f64) * 100f64;
+        let units = convert_units(sent as f64, header.filesize as f64);
         print!(
-            "\r => {} of {} bytes ({:.02}%)",
-            sent, header.filesize, percent
+            "\r => {:.03}{} of {:.03}{} ({:.02}%)",
+            units.partial, units.partial_unit, units.total, units.total_unit, percent
         );
         io::stdout().flush().unwrap();
     }
@@ -176,7 +183,7 @@ fn recv(mut stream: TcpStream) -> Result<()> {
     // Receive file data
     let mut file = File::create(&header.filename).expect("Could not open file");
     let mut buf: [u8; 4096] = [0; 4096];
-    let mut received = 0;
+    let mut received: u64 = 0;
     loop {
         // Read from network connection
         let len = stream.read(&mut buf).expect("Failed to read");
@@ -195,14 +202,56 @@ fn recv(mut stream: TcpStream) -> Result<()> {
             break;
         }
 
-        received += len;
-        let percent: f64 = (received as u64 / header.filesize) as f64 * 100f64;
+        received += len as u64;
+        let percent: f64 = (received as f64 / header.filesize as f64) * 100f64;
+        let units = convert_units(received as f64, header.filesize as f64);
         print!(
-            "\r => {} of {} bytes ({:.02}%)",
-            received, header.filesize, percent
+            "\r => {:.03}{} of {:.03}{} ({:.02}%)",
+            units.partial, units.partial_unit, units.total, units.total_unit, percent
         );
         io::stdout().flush().unwrap();
     }
 
     Ok(())
+}
+
+fn convert_units(mut partial: f64, mut total: f64) -> SizeUnit {
+    let unit = ['B', 'K', 'M', 'G', 'T'];
+    let mut out = SizeUnit {
+        partial: 0.0,
+        partial_unit: 'B',
+        total: 0.0,
+        total_unit: 'B',
+    };
+
+    let mut count = 0;
+    loop {
+        if (total / 1024.0) > 1.0 {
+            count += 1;
+            total /= 1024.0;
+        } else {
+            break;
+        }
+        if count == unit.len() - 1 {
+            break;
+        }
+    }
+    out.total = total;
+    out.total_unit = unit[count];
+
+    count = 0;
+    loop {
+        if (partial / 1024.0) > 1.0 {
+            count += 1;
+            partial /= 1024.0;
+        } else {
+            break;
+        }
+        if count == unit.len() - 1 {
+            break;
+        }
+    }
+    out.partial = partial;
+    out.partial_unit = unit[count];
+    out
 }
